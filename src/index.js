6a8c40357a37188cf9682cf366f0bdb207e495ee
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-08-07 00:34:01 +0300

    feat: add function code to index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    // write your solution here
+    const arr = expr.trim().split('');
+	let leftBr = 0;
+	let rightBr = 0;
+	arr.forEach(i => {
+		if (i === '(') leftBr++;
+        if (i === ')') rightBr++; 
+    });
+    try {
+        const x = Function('return ' + expr)();
+        if (leftBr !== rightBr) {
+            throw new SyntaxError('ExpressionError: Brackets must be paired');
+        };
+        if (Function('return ' + expr)() === Infinity || expr.includes('/ 0')) {
+            throw new TypeError('TypeError: Division by zero.');
+        };
+        return x;
+    }
+    catch (e) {
+        if (e.name == 'SyntaxError') {
+            throw new SyntaxError('ExpressionError: Brackets must be paired');
+        } else if (e.name == 'TypeError') {
+            throw new TypeError("TypeError: Division by zero.");
+        }
+    }
 }
 
 module.exports = {
